e7e20f2130c94828a131569a9c686b481f547896
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-06-26 11:32:56 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SVPP_CS_WPF_Lab7_Characteristics_houses_Db_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
